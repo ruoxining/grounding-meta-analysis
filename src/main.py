a32feba6c04b1c8545175a126550e70223931ba1
Ruoxi Ning <ruoxining@outlook.com>
2025-03-04 03:18:53 -0500

    [fix] fix project prototypoe

---
+++ src/main.py
@@ -0,0 +1,10 @@
+"""The main module of the application."""
+from src.utils.paper_dataset import PaperDataset
+from src.utils.querier import Querier
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
