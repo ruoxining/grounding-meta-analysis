361fc29cd50be2ecce82b372a36deccfa34af3e4
Ruoxi Ning <ruoxining@outlook.com>
2025-03-18 23:21:03 -0400

    [feat] clean up dataset class

--- src/utils/paper_dataset.py
+++ src/utils/paper_dataset.py
@@ -2,14 +2,11 @@
 import logging
 import re
 from collections import defaultdict
-from typing import Dict, List, Tuple
+from typing import Any, Dict, List
 
+import bertopic
 import datasets
 import nltk
-import tqdm
-from bertopic import BERTopic
-
-from utils.querier import Querier
 
 
 class PaperDataset:
@@ -17,10 +14,7 @@ class PaperDataset:
         """Initialize the PaperDataset class."""
         # Dict[str, datasets.arrow_dataset.Dataset]
         self._ds = datasets.load_dataset("Seed42Lab/AI-paper-crawl")
-        self._querier = Querier(model_name='gpt-4', api_key='api_key') # TODO: to real model and api key
         self._keyword = keyword
-        self._lemmatizer = nltk.stem.WordNetLemmatizer()
-        self._stopwords = set(nltk.corpus.stopwords.words('english'))
 
     @property
     def conference_list(self) -> List[str]:
@@ -31,159 +25,200 @@ class PaperDataset:
     def keyword(self) -> str:
         """Return the keyword."""
         return self._keyword
-    
+
     def get_conference(self, conference_name: str) -> List[str]:
         """Return the dataset for the given conference."""
-        # TODO: to see if correct
         return self._ds[conference_name]
     
-    def get_paper_by_keyword(self,
-                             conference_name: list = None,
-                             section: str = 'abstract'
-                             ) -> Dict[str, List[datasets.arrow_dataset.Dataset]]:
-        """Return the retrieved papers by keyword.
-        
+    def filter_papers(self,
+                      conference_name: str = None,
+                      in_full_text: bool = True,
+                      get_section: str = 'full_text'
+                      ) -> Dict[str, List[datasets.arrow_dataset.Dataset]]:
+        """Filter the papers based on the keyword.
+
         Args:
-            keyword         : keyword to be searched for.
-            conference_name : specify a list of conference names to search for, default all.
-            section         : specify the section to search for, default 'abstract', choices: ['abstract', 'title', 'full_text'].
+            conference_name     : conference to search for.
+            in_full_text        : if the keyword must be in the full text or only in the abstract.
+            get_section         : the section to get, 'abstract', 'full_text', 'main_text', 'reference'.
 
         Returns:
-            papers          : a dictionary of papers retrieved by keyword.
+            papers              : the papers retrieved.
         """
         # configs
-        papers = defaultdict(list)
         if conference_name is None:
             conference_name = self.conference_list
-        assert section in ['abstract', 'title', 'full_text'], "Invalid section."        
 
-        def get_section(paper, section):
-            if section == 'abstract':
-                # TODO: some paper does not have abstract
-                try:
-                    return paper['text'].lower().split('introduction')[0].split('abstract')[1]
-                except:
-                    return ''
-            elif section == 'title':
-                return paper['text'].lower().split('abstract')[0]
-            elif section == 'full_text':
-                return paper['text'].lower()
-
-        # main loop
+        # get papers
+        logging.info(f"Filtering the papers with the keyword '{self._keyword}'.")
+        papers = defaultdict(list)
         for conference in conference_name:
             for paper in self._ds[conference]:
-                if self.keyword in get_section(paper, section):
+                text = self._segment_text(paper['text'], 'full_text' if in_full_text else 'abstract')
+                if self._keyword in text:
                     papers[conference].append(paper)
 
-        return papers
+        # filter sections
+        logging.info(f"Segmenting the paper text into {get_section}.")
+        papers_filtered = defaultdict(list)
+        for conference in conference_name:
+            for paper in papers[conference]:
+                paper_tmp = paper.copy()
+                text = self._segment_text(paper['text'], get_section)
+                paper_tmp['text'] = text
+                papers_filtered[conference].append(paper_tmp)
 
-    def get_related_words(self, window_size: int, top_n: int) -> Dict[str, List[Tuple[str, int]]]:
-        """Get the related words for the given keyword with LLMs.
-        
-        Args:
-            window_size     : window size to search from.
-            top_n           : number of top related words to return.
+        return papers_filtered
 
-        Returns:
-            related_words   : the list of related words.
-        """
-        logging.info('Getting related words...')
-        paper_by_conference = self.get_paper_by_keyword(section='full_text')
-        related_words = {}
-
-        logging.info('Building word frequency...')
-        for conference, papers in paper_by_conference.items():
-            logging.info(f'Processing conference: {conference}')
-            word_freq = defaultdict(int)
-
-            for paper in tqdm.tqdm(papers):
-                text = paper['text'].lower()
-                sentences = nltk.tokenize.sent_tokenize(text)
-
-                for sentence in sentences:
-                    if self.keyword in sentence:
-                        words = nltk.tokenize.word_tokenize(sentence)
-                        keyword_indices = [i for i, word in enumerate(words) if word == self.keyword]
-
-                        for idx in keyword_indices:
-                            start = max(0, idx - window_size)
-                            end = min(len(words), idx + window_size + 1)
-
-                            # get related words in the window
-                            for i in range(start, end):
-                                if i != idx:  # skip the keyword itself
-                                    word = words[i]
-                                    word = re.sub(r'[^\w\s]', '', word)
-                                    if word and word not in self._stopwords:
-                                        word = self._lemmatizer.lemmatize(word)
-                                        word_freq[word] += 1
-
-            # TODO: better logging everywhere
-
-            # get top n words
-            top_words = sorted(word_freq.items(), key=lambda x: x[1], reverse=True)[:top_n]
-            related_words[conference] = top_words
-    
-        # Conf: List[(word, freq)]
-
-        return related_words
-
-    def get_related_topics(self, 
-                           min_topic_size: int = 10, 
-                           nr_topics: str = 'auto'
-                           ) -> List[str]:
-        """Get the related topics for the given keyword with BERTopic.
+    def _segment_text(self, 
+                      text: str, 
+                      get_section: str
+                      ) -> Dict[str, List[str]]:
+        """Segment the text into sentences.
 
         Args:
-            min_topic_size  : minimum size of topics.
-            nr_topics       : number of topics to extract, 'auto' for automatic detection.
+            text                : the text to segment.
+            get_section         : the section to get, 'abstract', 'full_text', 'main_text', 'references'.
 
         Returns:
-            related_topics  : the list of related topics.
+            papers              : the papers retrieved.
         """
-        papers_by_conf = self.get_paper_by_keyword(section='abstract')
-        results = {}
+        if get_section == 'abstract':
+            # find beginning
+            begin_idx = text.lower().find('abstract')
+            if begin_idx == -1:
+                return ''
+            # find ending
+            text_tmp = text[begin_idx:]
+            while 'keywords' in text_tmp.lower():
+                text_tmp = text_tmp[:text_tmp.lower().find('keywords')]
+            while 'introduction' in text_tmp.lower():
+                text_tmp = text_tmp[:text_tmp.lower().find('introduction')]
+            return text_tmp
+        elif get_section == 'full_text':
+            return text
+        elif get_section == 'main_text':
+            # find beginning         
+            begin_idx = text.lower().find('introduction')
+            if begin_idx == -1:
+                begin_idx = 0
+            # find ending
+            end_idx = text.lower().find('reference')
+            if end_idx == -1:
+                end_idx = len(text)
+            return text[begin_idx:end_idx]
+        elif get_section == 'reference':
+            # find beginning
+            text_tmp = text
+            while 'references' in text_tmp.lower():
+                text_tmp = text_tmp[text_tmp.lower().find('reference')+len('references'):]
+            while 'reference' in text_tmp.lower():
+                text_tmp = text_tmp[text_tmp.lower().find('reference')+len('reference'):]
+            return text_tmp
+        else:
+            raise ValueError("Invalid section.")
 
-        for conf, papers in papers_by_conf.items():
-            # get abstracts
-            abstracts = []
-            for paper in papers:
-                text = paper['text'].lower()
-                if 'abstract' in text and 'introduction' in text:
-                    abstract = text.split('introduction')[0].split('abstract')[1].strip()
-                    abstracts.append(abstract)
+    def get_topics(self,
+                   conference_name: list = None,
+                   in_full_text: bool = True,
+                   min_topic_size: int = 10,
+                   nr_topics: int = 'auto'
+                   ) -> Dict[str, Any]:
+        """Get the topics in the papers with BERTopic.
 
-            if len(abstracts) < min_topic_size:
-                results[conf] = {"status": "Not enough papers."}
+        Args:
+            conference_name     : conference to search for, default all.
+            in_full_text        : if the keyword must be in the full text or only in the abstract.
+            min_topic_size      : minimum size of the topic.
+            nr_topics           : the number of topics to get.
 
-            # create and fit BERTopic model
-            topic_model = BERTopic(min_topic_size=min_topic_size, nr_topics=nr_topics)
-            topics, _ = topic_model.fit_transform(abstracts)
-
-            # get topic information
-            topic_info = topic_model.get_topic_info()
-            topic_representations = {}
-
-            for topic_id in set(topics):
-                # skip        
-                if topic_id != -1:
-                    topic_words = topic_model.get_topic(topic_id)
-                    topic_representations[topic_id] = topic_words
-
-            results[conf] = {
-                "status": "success",
-                "topic_info": topic_info,
-                "topic_representations": topic_representations,
-                "topics": topics,
-                "model": topic_model
-            }
-
-        return results
-
-    def extract_related_papers(self) -> Dict[str, List[Dict[str, str]]]:
-        """Extract the related papers for the given keyword.
-        
         Returns:
-            related_papers  : the related papers, both references and derivations.
+            topics              : the topics in the papers.
         """
+        # configs
+        if conference_name is None:
+            conference_name = self.conference_list
+
+        # build topic model
+        logging.info("Building the topic model.")
+        topic_model = bertopic.BERTopic(min_topic_size=min_topic_size, nr_topics=nr_topics)
+
+        # get papers
+        papers = self.get_paper_full_text(conference_name, in_full_text)
+
+        # main loop
+        topics = defaultdict(list)
+        logging.info("Extracting the topics.")
+        topics = defaultdict(list)
+        for conf in papers:
+            topics[conf] = []
+            for paper in papers[conf]:
+                topics, _ = topic_model.fit_transform(paper['text'])
+                topic_info = topic_model.get_topic_info()
+                topic_reprensentation = {}
+                for topic_id in set(topics):
+                    if topic_id != -1:
+                        topic_words = topic_model.get_topic(topic_id)
+                        topic_reprensentation[topic_id] = topic_words
+                topics[conf].append({
+                    'paper_id': paper['index'],
+                    'topic_info': topic_info,
+                    'topic_reprensentation': topic_reprensentation
+                    'topics': topics
+                    })
+        return topics
+
+    def get_related_words(self,
+                          conference_name: list = None,
+                          in_full_text: bool = True,
+                          method: str = 'llm',
+                          ) -> Dict[str, Dict[str, float]]:
+        """Get the related words for the given keyword.
+
+        Args:
+            conference_name     : conference to search for, default all.
+            in_full_text        : if the keyword must be in the full text or only the abstract.
+            method      : the method to use, 'llm', 'sliding_window', 'parsing'.
+
+        Returns:
+            related_words       : the related words, in conf: keyword: freq.
+        """
+        # configs
+        if conference_name is None:
+            conference_name = self.conference_list
+
         pass
+
+    def get_percent_numbers(self,
+                            conference_name: list = None,
+                            in_full_text: bool = True,
+                            ) -> Dict[str, float]:
+        """Get the percentage of numbers in selected papers.
+
+        Args:
+            conference_name     : conference to search for, default all.
+            in_full_text        : if the keyword must be in the full text.
+
+        Returns:
+            percent_numbers     : the percentage of numbers.
+        """
+        # configs
+        if conference_name is None:
+            conference_name = self.conference_list
+
+        # TODO: exclude the functional numbers, i.e., the numbers in the author list and references
+        pass
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description='Main module of the application.')
+    parser.add_argument('--keyword', '-k', type=str, help='The keyword to search for.')
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO)
+
+    paper_dataset = PaperDataset(keyword=args.keyword)
+
+    papers = paper_dataset.filter_papers(conference_name=['ACL'], in_full_text=True, get_section='reference')
+
